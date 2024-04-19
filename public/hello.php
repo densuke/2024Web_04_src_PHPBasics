fba83b0e77517f6eb1d9941072932d35aca5ba5a
SATO Daisuke <densuke@st.kobedenshi.ac.jp>
2024-04-20 04:48:59 +0900

    public以下を整理

---
+++ public/hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<!-- 今回は初回なので、仮のファイルを用意しましたが、今後は指示に従い、
+    自分でファイルを作っていって下さい -->
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- この領域(bodyタグ内)に処理内容を入れてください -->
+    <p><?php echo 'Hello,PHP'; ?></p>
+</body>
+</html>
